6f9a59ed66d44901f52fe0ec35de75549d7241e4
Pietro Alves <pietrorodrigues@gmail.com>
2020-03-25 14:18:50 -0300

    CRUD Scaffolding Department

---
+++ SalesWebMvcCourse/Data/SalesWebMvcCourseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesWebMvcCourse.Models;
+
+namespace SalesWebMvcCourse.Data
+{
+    public class SalesWebMvcCourseContext : DbContext
+    {
+        public SalesWebMvcCourseContext (DbContextOptions<SalesWebMvcCourseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesWebMvcCourse.Models.Department> Department { get; set; }
+    }
+}
